60fd144090e367308ad4a18e768ea2e8c6925c18
Gustavo Henriques <gustavohenrique13579@aluno.ufsj.edu.br>
2024-03-26 16:12:26 -0300

    comecando a calcular a qualidade

--- alg_genetico_nreal.py
+++ alg_genetico_nreal.py
@@ -1,18 +1,20 @@
+from io import TextIOWrapper
 import math
 import random
 import copy
+import numpy
 import numpy.random as npr
 
-def cria_populacao_inicial(x_min:int, x_max:int, npop:int) -> list :
+def cria_populacao_inicial(x_min:int, x_max:int, npop:int, x_n:int) -> list :
     pop = list()
     for i in range(npop):
         indiv_temp = list()
-        for j in range(3):
+        for j in range(x_n):
             indiv_temp.append(random.uniform(x_min, x_max))
         pop.append(indiv_temp)
     return pop
 
-def avalia_populacao(pop:list) -> list:
+def avalia_populacao(pop:list, x_n:int) -> list:
     fit = []
     for x in pop:
         x_sum = 0
@@ -21,7 +23,7 @@ def avalia_populacao(pop:list) -> list:
         cos_sum = 0
         for x_i in x:
             cos_sum += math.cos(2*math.pi*x_i)
-        f_x = -20*(math.e**(-0.2*math.sqrt((1/3)*x_sum))) - math.e**((1/3)*cos_sum) + 20 + math.e
+        f_x = -20*(math.e**(-0.2*math.sqrt((1/x_n)*x_sum))) - math.e**((1/x_n)*cos_sum) + 20 + math.e
         fit.append(f_x)
     return fit
 
@@ -42,8 +44,33 @@ def seleciona_pais(npop:int, fit:list, pop:list, num_pais:int) -> list:
         cont += 1
     
     return pais
+
+def cruzamento_blx_alfa(pais:list, pop:list, pc:float, alfa:float) -> list:
+    pop_intermediaria = []
+    for p1, p2 in zip(*[iter(pais)]*2):
+        r = random.random()
+        if r > pc:
+            pop_intermediaria.append(pop[p1])
+            pop_intermediaria.append(pop[p2])
+            continue
+        if fit[p1] <= fit[p2]:
+            x = pop[p1]
+            y = pop[p2]
+        else:
+            x = pop[p2]
+            y = pop[p1]
+        d = [abs(x_i - y_i) for x_i,y_i in zip(x,y)]
+        f1 = []
+        f2 = []
+        for x_i, y_i, d_i in zip(x,y,d):
+            f1.append(random.uniform(min(x_i, y_i)-alfa*d_i, max(x_i, y_i)+alfa*d_i))
+            f2.append(random.uniform(min(x_i, y_i)-alfa*d_i, max(x_i, y_i)+alfa*d_i))
+                
+        pop_intermediaria.append(f1)
+        pop_intermediaria.append(f2)
+    return pop_intermediaria
     
-def cruzamento(pais:list, pop:list, pc:float, alfa:float, beta:float) -> list:
+def cruzamento_blx_alfa_beta(pais:list, pop:list, pc:float, alfa:float, beta:float) -> list:
     pop_intermediaria = []
     for p1, p2 in zip(*[iter(pais)]*2):
         r = random.random()
@@ -83,6 +110,18 @@ def mutacao(pop:list, pm:float, x_min:int, x_max:int) -> list:
         pop_intermediaria.append(temp_inv)
     return pop_intermediaria
 
+def mutacao_por_pertubacao(pop:list, pm:float, pertubacao:float) -> list:
+    pop_intermediaria = []
+    for i in pop:
+        temp_inv = []
+        for j in i:
+            r = random.random()
+            if(r<=pm):
+                j = j * pertubacao
+            temp_inv.append(j)
+        pop_intermediaria.append(temp_inv)
+    return pop_intermediaria
+
 def elitismo(pop:list, fit:list, nelite:int) -> list:
     elites = []
     sorted_list = sorted(fit, reverse=False)
@@ -93,11 +132,31 @@ def elitismo(pop:list, fit:list, nelite:int) -> list:
         elites.append(pop[pos])
     print()
     return elites
+
+def write_pop_in_archive(pop:list, fit:list, f:TextIOWrapper):
+    sorted_list = sorted(fit, reverse=False)
+    for i in range(len(pop)):
+        pos = fit.index(sorted_list[i])
+        f.write("%lf, %lf, %lf -> %lf\n" %(pop[pos][0], pop[pos][1], pop[pos][2], sorted_list[i]))
+
+def write_best_in_archive(pop:list, fit:list, f:TextIOWrapper):
+    sorted_list = sorted(fit, reverse=False)
+    melhor = sorted_list[0]
+    pior = sorted_list[len(sorted_list) - 1]
+    media = numpy.average(sorted_list)
+    desvio = numpy.std(sorted_list)
+    f.write("%lf;%lf;%lf;%lf\n" %(melhor, pior, media, desvio))
     
 
-npop = 100 # tem que ser par
+best_per_generation_file = open("best_per_generation.txt", "w")
+popualation_per_generation_file = open("popualation_per_generation.txt", "w")
+
+best_per_generation_file.write("geracao;melhor;pior;media;desvio\n")
+
 nger = 100
-nelite = 2 # tem que ser par
+# npop + nelite = par
+npop = 100
+nelite = 2
 
 pop = []
 pop_intermediaria = []
@@ -111,18 +170,24 @@ beta = 0.7
 
 pm = 0.05
 
-x_min = -2
+x_min = -2 
 x_max = 2
+x_n = 3 # numero de termos no somatorio
 
-pop = cria_populacao_inicial(x_min, x_max, npop)
+pop = cria_populacao_inicial(x_min, x_max, npop, x_n)
 
 geracoes = 0
 while(geracoes != nger):
     print("Geracao: ", geracoes)
-    fit = avalia_populacao(pop)
+    fit = avalia_populacao(pop, x_n)
+
+    best_per_generation_file.write("%d;" %(geracoes))
+    popualation_per_generation_file.write("\nGeracao: %d\n" %(geracoes))
+    write_best_in_archive(pop, fit, best_per_generation_file)
+    write_pop_in_archive(pop, fit, popualation_per_generation_file)
 
     pais = seleciona_pais(npop, fit, pop, npop-nelite)
-    pop_intermediaria = cruzamento(pais, copy.deepcopy(pop), pc, alfa, beta)
+    pop_intermediaria = cruzamento_blx_alfa_beta(pais, copy.deepcopy(pop), pc, alfa, beta)
 
     pop_intermediaria = mutacao(pop_intermediaria, pm, x_min, x_max)
 
@@ -140,3 +205,5 @@ print()
 print("Melhor: ", best)
 print("Fit: ", best_solution)
 
+best_per_generation_file.close()
+popualation_per_generation_file.close()
